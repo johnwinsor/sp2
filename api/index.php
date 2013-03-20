72fe578cde09cf84090197f338a80e76bd8e121f
John Winsor <jwinsor@mills.edu>
2013-03-20 09:37:27 -0700

    Clean Install Version 2.0 (Beta). Copied user files

---
+++ api/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *   @file api/index.php
+ *   @brief Return data from subjectsplus
+ *	 @package SubjectsPlus API
+ *
+ *   @author dgonzalez
+ *   @date November 2012
+ */
+
+include_once("../control/includes/config.php");
+include_once("../control/includes/functions.php");
+
+//if the not properly configured, redirect to control folder for installation/update
+if( !isset( $BaseURL ) )
+{
+	header( "Location: ../control/" );
+	exit;
+}
+
+$lobjWebServiceHandler = new sp_WebServiceHandler($uname, $pword, $dbName_SPlus, $hname);
+
+$lobjWebServiceHandler->doService();
+
+$lobjWebServiceHandler->displayOutput();
+?>
